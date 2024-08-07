39a98ada40511497573b16100fdfe77eb9f0d654
Admin <neketobro@gmail.com>
2024-08-07 13:56:33 +0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+//form
+var form_box = document.getElementById('form-box');
+
+setTimeout(function(){
+    form_box.style.display = "block";
+}, 10000)
+
+function btnClick(el) {
+    form_box.style.display = "none";
+}
+
+
